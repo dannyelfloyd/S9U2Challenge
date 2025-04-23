121b34283634fcb9740a090fef686325970a7265
dannyelfloyd <dannyelfloyd@gmail.com>
2025-04-23 13:07:27 +0200

    REVIEW

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const http = require('node:http');
 const object = require('./data.js');
 const server = http.createServer((req, res) => {
-  console.log('request recived');
   res.end(`
     <h1>${object.title}</h1>
     <h2>${object.subtitle}</h2>
     <p>${object.description}</p>
     `);
 });
-server.listen(0, () => {
+const PORT = 3000;
+server.listen(PORT, () => {
   console.log(
-    `server listening on port http://localhost:${server.address().port}`
+    `server listening on port http://localhost:${PORT}`
   );
 });
+//Terminal: node --watch server.js
